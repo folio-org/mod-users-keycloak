79a148c9b94efd64b06b0d7a034268147cdb50cc
BKadirkhodjaev <b.kadirkhodjaev@gmail.com>
2025-02-27 13:40:06 +0500

    [MODUSERSKC-75]. Simplify linkUserIdpMigration

--- src/main/java/org/folio/uk/migration/IdpMigrationService.java
+++ src/main/java/org/folio/uk/migration/IdpMigrationService.java
@@ -4,7 +4,6 @@ import static org.apache.commons.collections4.ListUtils.partition;
 import static org.folio.spring.scope.FolioExecutionScopeExecutionContextManager.getRunnableWithCurrentFolioContext;
 import static org.folio.spring.utils.FolioExecutionContextUtils.prepareContextForTenant;
 
-import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -19,8 +18,6 @@ import org.folio.spring.FolioExecutionContext;
 import org.folio.spring.FolioModuleMetadata;
 import org.folio.spring.scope.FolioExecutionContextSetter;
 import org.folio.uk.domain.dto.UsersIdp;
-import org.folio.uk.domain.entity.EntityUserMigrationJobStatus;
-import org.folio.uk.domain.entity.UserMigrationJobEntity;
 import org.folio.uk.integration.keycloak.KeycloakService;
 import org.folio.uk.migration.properties.UserMigrationProperties;
 import org.folio.uk.service.UserService;
@@ -48,45 +45,35 @@ public class IdpMigrationService {
       throw new IllegalStateException("Cannot link users to IDP, no userIds is supplied in request body");
     }
 
-    try (var ignored = new FolioExecutionContextSetter(
-      // Should switch to a central tenant in order to fetch shadow users of the incoming staff user ids
-      prepareContextForTenant(tenantId, folioModuleMetadata, folioExecutionContext))) {
-      log.info("Linking {} user(s) in {} tenant", userIds.size(), tenantId);
+    log.info("Linking {} user(s) in {} tenant", userIds.size(), tenantId);
 
-      var job = buildUserIdpMigrationsEntity();
+    // Should switch to a central tenant in order to fetch shadow users of the incoming staff user ids
+    try (var ignored = new FolioExecutionContextSetter(
+      prepareContextForTenant(tenantId, folioModuleMetadata, folioExecutionContext))) {
+
       var partitions = partition(userIds, migrationProperties.getBatchSize());
       CompletableFuture.allOf(partitions.stream()
           .map(part -> CompletableFuture.runAsync(getRunnableWithCurrentFolioContext(
             () -> findAndLinkUserIdpByPart(part))))
           .toArray(CompletableFuture[]::new))
-        .whenComplete(migrationCompleteHandler(job));
+        .whenComplete(migrationCompleteHandler(userIds.size()));
     }
   }
 
-  private UserMigrationJobEntity buildUserIdpMigrationsEntity() {
-    var migration = new UserMigrationJobEntity();
-    migration.setId(UUID.randomUUID());
-    migration.setStatus(EntityUserMigrationJobStatus.IN_PROGRESS);
-    migration.setStartedAt(Instant.now());
-    return migration;
-  }
-
   private void findAndLinkUserIdpByPart(List<UUID> userIds) {
-    log.info("Linking {} user(s) part in {} tenant", userIds.size(), folioExecutionContext.getTenantId());
     userIds.forEach(userId ->
       userService.getUser(userId).ifPresent(user ->
         keycloakService.findKeycloakUserWithUserIdAttr(user.getId()).ifPresent(keycloakUser ->
           keycloakService.linkIdentityProviderToUser(user, keycloakUser.getId()))));
   }
 
-  private BiConsumer<Void, ? super Throwable> migrationCompleteHandler(UserMigrationJobEntity job) {
+  private BiConsumer<Void, ? super Throwable> migrationCompleteHandler(int totalRecords) {
     return (result, ex) -> {
       if (Objects.isNull(ex)) {
-        log.info("User IDP Migration has successfully finished. id: {}, total Records: {}", job.getId(),
-          job.getTotalRecords());
+        log.info("User IDP linking Migration has finished, total records: {}", totalRecords);
         return;
       }
-      log.error("User IDP Migration has failed, id: {}", job.getId(), ex);
+      log.error("User IDP linking Migration has failed", ex);
     };
   }
 }
