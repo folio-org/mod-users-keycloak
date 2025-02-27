10f36a7b617a00519503c31324031ed690d6fc00
BKadirkhodjaev <b.kadirkhodjaev@gmail.com>
2025-02-27 14:09:30 +0500

    [MODUSERSKC-75]. Add central tenant validation and env var controls

--- src/main/java/org/folio/uk/migration/IdpMigrationService.java
+++ src/main/java/org/folio/uk/migration/IdpMigrationService.java
@@ -2,7 +2,6 @@ package org.folio.uk.migration;
 
 import static org.apache.commons.collections4.ListUtils.partition;
 import static org.folio.spring.scope.FolioExecutionScopeExecutionContextManager.getRunnableWithCurrentFolioContext;
-import static org.folio.spring.utils.FolioExecutionContextUtils.prepareContextForTenant;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,10 +14,9 @@ import lombok.extern.log4j.Log4j2;
 import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.folio.spring.FolioExecutionContext;
-import org.folio.spring.FolioModuleMetadata;
-import org.folio.spring.scope.FolioExecutionContextSetter;
 import org.folio.uk.domain.dto.UsersIdp;
 import org.folio.uk.integration.keycloak.KeycloakService;
+import org.folio.uk.integration.keycloak.config.KeycloakFederatedAuthProperties;
 import org.folio.uk.migration.properties.UserMigrationProperties;
 import org.folio.uk.service.UserService;
 import org.springframework.stereotype.Service;
@@ -30,34 +28,38 @@ public class IdpMigrationService {
 
   private final UserService userService;
   private final KeycloakService keycloakService;
-  private final FolioModuleMetadata folioModuleMetadata;
   private final FolioExecutionContext folioExecutionContext;
   private final UserMigrationProperties migrationProperties;
+  private final KeycloakFederatedAuthProperties keycloakFederatedAuthProperties;
 
   public void linkUserIdpMigration(UsersIdp usersIdp) {
+    if (Boolean.FALSE.equals(keycloakFederatedAuthProperties.isEnabled())) {
+      log.info("Linking users to an IDP is disabled");
+      return;
+    }
+
     var tenantId = usersIdp.getTenantId();
     if (StringUtils.isEmpty(tenantId)) {
-      throw new IllegalStateException("Cannot link users to IDP, no tenant id is supplied in request body");
+      throw new IllegalStateException("Cannot link users to an IDP, no tenant id is supplied in request body");
+    }
+
+    if (!StringUtils.equals(folioExecutionContext.getTenantId(), tenantId)) {
+      throw new IllegalStateException("Cannot link users to an IDP, supplied tenantId doesn't match context tenantId");
     }
 
     var userIds = new ArrayList<>(usersIdp.getUserIds());
     if (CollectionUtils.isEmpty(userIds)) {
-      throw new IllegalStateException("Cannot link users to IDP, no userIds is supplied in request body");
+      throw new IllegalStateException("Cannot link users to an IDP, no userIds is supplied in request body");
     }
 
     log.info("Linking {} user(s) in {} tenant", userIds.size(), tenantId);
 
-    // Should switch to a central tenant in order to fetch shadow users of the incoming staff user ids
-    try (var ignored = new FolioExecutionContextSetter(
-      prepareContextForTenant(tenantId, folioModuleMetadata, folioExecutionContext))) {
-
-      var partitions = partition(userIds, migrationProperties.getBatchSize());
-      CompletableFuture.allOf(partitions.stream()
-          .map(part -> CompletableFuture.runAsync(getRunnableWithCurrentFolioContext(
-            () -> findAndLinkUserIdpByPart(part))))
-          .toArray(CompletableFuture[]::new))
-        .whenComplete(migrationCompleteHandler(userIds.size()));
-    }
+    var partitions = partition(userIds, migrationProperties.getBatchSize());
+    CompletableFuture.allOf(partitions.stream()
+        .map(part -> CompletableFuture.runAsync(getRunnableWithCurrentFolioContext(
+          () -> findAndLinkUserIdpByPart(part))))
+        .toArray(CompletableFuture[]::new))
+      .whenComplete(migrationCompleteHandler(userIds.size()));
   }
 
   private void findAndLinkUserIdpByPart(List<UUID> userIds) {
@@ -69,11 +71,11 @@ public class IdpMigrationService {
 
   private BiConsumer<Void, ? super Throwable> migrationCompleteHandler(int totalRecords) {
     return (result, ex) -> {
-      if (Objects.isNull(ex)) {
-        log.info("User IDP linking Migration has finished, total records: {}", totalRecords);
+      if (Objects.nonNull(ex)) {
+        log.error("User IDP linking Migration has failed", ex);
         return;
       }
-      log.error("User IDP linking Migration has failed", ex);
+      log.info("User IDP linking Migration has finished, total records: {}", totalRecords);
     };
   }
 }
