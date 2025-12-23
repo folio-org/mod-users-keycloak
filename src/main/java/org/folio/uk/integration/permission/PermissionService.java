a49088b38b6ad55f52f2e2d01a3bd55e40b7958c
BKadirkhodjaev <b.kadirkhodjaev@gmail.com>
2025-12-23 18:23:05 +0500

    [MODUSERSKC-116]. Fix checkstyle

--- src/main/java/org/folio/uk/integration/permission/PermissionService.java
+++ src/main/java/org/folio/uk/integration/permission/PermissionService.java
@@ -5,7 +5,6 @@ import static org.apache.commons.collections4.CollectionUtils.isEmpty;
 
 import java.util.LinkedHashSet;
 import java.util.stream.Collectors;
-
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.folio.uk.integration.permission.model.PermissionUser;
