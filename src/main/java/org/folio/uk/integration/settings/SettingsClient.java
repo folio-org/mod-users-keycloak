3333b420a2cd915c3ccf2de0b69849d5f865f029
PK Jacob <pkjacob@ebsco.com>
2026-04-15 15:15:29 -0400

    MODUSERSKC-140 Move base-url path to method exchange

--- src/main/java/org/folio/uk/integration/settings/SettingsClient.java
+++ src/main/java/org/folio/uk/integration/settings/SettingsClient.java
@@ -4,7 +4,7 @@ import org.folio.uk.integration.settings.model.BaseUrlResponse;
 import org.springframework.web.service.annotation.GetExchange;
 import org.springframework.web.service.annotation.HttpExchange;
 
-@HttpExchange(url = "settings")
+@HttpExchange
 public interface SettingsClient {
 
   @GetExchange("base-url")
